eafc5c92af8aade7b40f15d4aedc952c1df7ce2d
Razenerau <molokov.phil@gmail.com>
2025-06-30 16:44:32 -0400

    arstrsatrat

---
+++ Assets/Scripts/cutscene.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class cutscene : MonoBehaviour
+{
+    [SerializeField] private GameObject scene1;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { }
+            
+              
+                scene1.gameObject.SetActive(false);
+            
+            
+        }
+    }
+}
