35ffb11775e3767920a9b2cb33760ab65744b835
Razenerau <molokov.phil@gmail.com>
2025-07-26 17:58:32 -0400

    made landing sound less frequent

--- Assets/PlayOnEnter.cs
+++ Assets/PlayOnEnter.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class PlayOnEnter : StateMachineBehaviour
 {
     [SerializeField] private SoundType _sound;
+    [SerializeField] private bool _hasRequirements = false;
+    [SerializeField] private float _requiredValue = 0;
+    [SerializeField] private string _requiredParameterName = "";
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        SoundManager.PlaySound(_sound);
+        float parameter = 999f;
+        if (_hasRequirements) parameter = animator.GetFloat(_requiredParameterName);
+
+        // Optional: Use requirements to conditionally play the sound
+        if (!_hasRequirements || parameter < _requiredValue)
+        {
+            SoundManager.PlaySound(_sound);
+        }
+
     }
 }
