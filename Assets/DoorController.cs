f5a4c4e073b332e1ac07b382fcd0bbec86707d08
Razenerau <molokov.phil@gmail.com>
2025-08-13 16:19:05 -0400

    Added new tiles

--- Assets/DoorController.cs
+++ Assets/DoorController.cs
@@ -13,19 +13,32 @@ public class DoorController : MonoBehaviour
         {
             Debug.Log("player not found");
             Player = GameObject.FindGameObjectWithTag("Player");
-
         }
 
-        if (OtherDoor != null)
-        {
-            Debug.Log("Teleported");
-            Player.transform.position = OtherDoor.transform.position;
-            SoundManager.PlaySound(SoundType.DOOR_OPEN);
-        }
-        else
+        if (OtherDoor == null)
         {
             SoundManager.PlaySound(SoundType.DOOR_LOCKED);
         }
+        else
+        {
+            StartCoroutine(TeleportAfterDelay());
+            Debug.Log("Teleported");
+            
+        }
         
     }
+
+    private IEnumerator TeleportAfterDelay()
+    {
+        SoundManager.PlaySound(SoundType.DOOR_OPEN);
+        PlayerController playerController = Player.GetComponent<PlayerController>();
+        playerController.enabled = false;
+        Player.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        Player.GetComponent<PlayerModel>().IsGrounded = true;
+
+        yield return new WaitForSeconds(1f); // Wait for 1 second
+        Player.transform.position = OtherDoor.transform.position;
+        playerController.enabled = true;
+    }
+
 }
