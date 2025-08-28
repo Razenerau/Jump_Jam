4c6979ac7a0f92b4afdc68f7de66d5e0b28e24af
Razenerau <molokov.phil@gmail.com>
2025-08-27 20:58:51 -0400

    added functions to the pause menu

--- Assets/PauseMenuController.cs
+++ Assets/PauseMenuController.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenuController : MonoBehaviour
 {
     public static bool IsPaused = false;
     public GameObject PauseMenu;
     public GameObject Player;
+    public Slider Slider;
+    public AudioSource Music;
 
     
     void Start()
     {
-        
+        SetPaused();
+        SetMusicVolume(0.5f);
     }
 
     void Update()
@@ -41,5 +46,20 @@ public class PauseMenuController : MonoBehaviour
             rb.isKinematic = false;
             rb.velocity = playerModel.LastVelocity;
         }
+    }
+
+    public void LoadedMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void SetMusicVolume()
+    {
+        Music.volume = Slider.value;
+    }
+    public void SetMusicVolume(float value)
+    {
+        Music.volume = value;
+        Slider.value = value;
     }
 }
